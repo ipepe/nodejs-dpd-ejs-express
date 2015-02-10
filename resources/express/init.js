ca7b1ad965503c7c546cf09b6f79a1bfc505db5f
Patryk Ptasinski <patryk.ptasinski@ipepe.pl>
2015-02-10 20:52:04 +0100

    Full login functionality with regsitering and members area

--- resources/express/init.js
+++ resources/express/init.js
@@ -1,4 +1,14 @@
 app.get('/', function(req, res) {
-	//console.log(req.me);
-    res.render('index.ejs', {res: res, req: req, user: req.me? req.me : false })
+	res.render('index.ejs', {res: res, req: req, me: req.me? req.me : false });
+});
+app.get('/index', function(req, res) {
+	res.render('index.ejs', {res: res, req: req, me: req.me? req.me : false });
+});
+
+app.get('/register', function(req, res) {
+	if(req.me){
+		res.redirect('/index')
+	}else{
+		res.render('register.ejs', {res: res, req: req, me: req.me? req.me : false });
+	}
 });
